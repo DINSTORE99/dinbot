2cfa3224765c46799899211f271e39b7362976ed
DINSTORE99 <dinstoree41@gmail.com>
2026-07-28 01:12:47 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,30 @@ import "./style.css";
 
 const API = "";
 
-function App() {
+function App() 
+useEffect(() => {
+  const audio = new Audio("/musik.mp3");
+  audio.loop = true;
+  audio.volume = 0.5;
+
+  audio.play().catch(() => {
+    console.log("Autoplay diblokir browser. Klik halaman untuk memulai musik.");
+  });
+
+  const startMusic = () => {
+    audio.play().catch(() => {});
+  };
+
+  document.addEventListener("click", startMusic, { once: true });
+
+  return () => {
+    audio.pause();
+    audio.currentTime = 0;
+    document.removeEventListener("click", startMusic);
+  };
+}, []);
+
+{
   const [page, setPage] = useState("dashboard");
 
   const [serverOnline, setServerOnline] = useState(false);
