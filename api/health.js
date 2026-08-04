e9330396adc9fa862301fa7a35fa78f583ce783a
DINSTORE99 <dinstoree41@gmail.com>
2026-08-04 21:24:32 +0700

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,8 +1,21 @@
 export default function handler(req, res) {
+  const uptime = Math.floor(process.uptime());
+
+  const hari = Math.floor(uptime / 86400);
+  const jam = Math.floor((uptime % 86400) / 3600);
+  const menit = Math.floor((uptime % 3600) / 60);
+  const detik = uptime % 60;
+
   res.status(200).json({
     success: true,
     server: "online",
     service: "DIN BOT API",
-    timestamp: Date.now()
+    timestamp: Date.now(),
+    uptime: {
+      hari,
+      jam,
+      menit,
+      detik
+    }
   });
 }
